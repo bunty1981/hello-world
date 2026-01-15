c32ad0251e05adc8234065fbd0e34d7fa82fe0f3
bunty1981 <roshni.b@gmail.com>
2026-01-14 20:47:29 -0600

    Test commit 1 for ci.yml check.

--- ci-cd-test/run-hello-world-v0.sh
+++ ci-cd-test/run-hello-world-v0.sh
@@ -4,5 +4,6 @@ source "/Users/BHAGARO1/anaconda3/etc/profile.d/conda.sh"
 conda activate first-env
 echo "In conda environment: $CONDA_DEFAULT_ENV"
 python ./hello-world/hello-world.py
+#
 conda deactivate
 echo "In conda environment: $CONDA_DEFAULT_ENV"
